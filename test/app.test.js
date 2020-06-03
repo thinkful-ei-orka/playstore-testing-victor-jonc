53558f8ab38712501f655b8002c752d15255b9c1
Victor Jarvis <vctrjrvs@gmail.com>
2020-06-03 15:27:58 -0400

    began adding tests, sort test remains

--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,37 @@ const { expect } = require('chai');
 const supertest = require('supertest');
 const app = require('../app');
 
-supertest(app).get('/apps').expect(200);
+describe('PlayStore App', () => {
+     it('should return a GET message', () => {
+          return supertest(app).get('/apps').expect(200);
+     })
 
+     it('should return an array of objects', () => {
+           return supertest(app)
+           .get('/apps')
+           .expect(200)
+           .expect('Content-Type', /json/)
+           .then(res => {
+                expect(res.body).to.be.an('array');
+           })
+     })
+
+     it('should GET /genre', () => {
+          return supertest(app)
+          .get('/apps')
+          .query({ genre: 'Action' })
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .then(res => {
+               expect(res.body).to.be.an('array');
+               console.log(res.body)
+               expect(res.body).to.have.lengthOf.at.least(1);
+               // expect(res.body).to.have.nested.property('Genres', 'Action');
+               res.body.forEach(obj => {
+                    console.log(obj['Genres'])
+                    expect(obj['Genres']).to.include('Action')
+               }) 
+          })
+    })
+})
 //TODO: Test if an array is coming back on the .expect(200, [body])
